60d4fa4f19c25bbc3d27a66aae3898949a091244
nfyn <971216008@qq.com>
2022-03-28 08:31:04 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    title: 'Django学习文档',
+    description: 'Django学习笔记',
+    base: '/learn-django/',
+    theme: 'reco',
+    markdown: {
+        lineNumbers: true,
+    },
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        subSidebar: 'auto',
+        nav: [
+            {text: '首页', link: '/'},
+            {
+                text: '南风以南的博客',
+                items: [
+                    {text: 'Github', link: 'https://github.com/nfyn'}
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false,
+                children: [
+                    {title: '学前必读', path: '/'}
+                ]
+            },
+            {
+                title: '基础学习',
+                path: '/pages/Introduction',
+                collapsable: false,
+                children: [
+                    {title: '简介', path: '/pages/Introduction'}
+                ]
+            }
+        ]
+    }
+  }
---
+++ docs/pages/Introduction.md
@@ -0,0 +1,14 @@
+---
+title: Django 简介
+author: 南风以南
+date: '2022-03-28'
+---
+
+
+## Django简介
+
+```python
+def main():
+    print('hello world')
+
+```
